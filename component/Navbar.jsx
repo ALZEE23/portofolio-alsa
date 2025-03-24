06551110645c115aa64a1c993c8fec2601d4110b
ALZEE23 <muhamadalfarizial726@gmail.com>
2025-03-24 17:41:43 +0700

    feat: setup lagi

---
+++ component/Navbar.jsx
@@ -0,0 +1,18 @@
+var React = require("react");
+var ReactDOM = require("react-dom");
+var style = require("../app.css");
+
+const cihuy = (
+  <div>
+    <h1 className={style.h1}>Hello World</h1>
+    <h2 className="center">Hello pack </h2>
+  </div>
+);
+
+const navbar = (
+  <div>
+    <h1>ini navbar</h1>
+  </div>
+);
+
+export { cihuy, navbar };
