ed93b545c348bdd99ff0cfe32605170f03ea1d0f
ALZEE23 <muhamadalfarizial726@gmail.com>
2025-04-08 10:32:57 +0700

    fix: styling

--- website/component/About.jsx
+++ website/component/About.jsx
@@ -5,28 +5,26 @@ var style = require("../app.css");
 const about = (
     <div id="about" className="container">
         <div className="box1">
-            <div className="lat"><div className="purple"></div></div>
-            <div className="initext">
+            <div className="purple"></div>
                 <p>Carlos Oscar Charles Carlos Oscar Charles Carlos Oscar Charles Carlos Oscar Charles Carlos Oscar Charles 
                 Carlos Oscar Charles Carlos Oscar Charles Carlos Oscar Charles Carlos Oscar Charles</p>
-            </div>
         </div>
 
         <div className="box2">
             <div className="green"></div>
-            <div className="initext">
+            
                 <p>Carlos Oscar Charles Carlos Oscar Charles Carlos Oscar Charles Carlos Oscar Charles Carlos Oscar Charles 
                 Carlos Oscar Charles Carlos Oscar Charles Carlos Oscar Charles Carlos Oscar Charles
                 </p>
-            </div>
+            
         </div>
 
         <div className="box3">
             <div className="pink"></div>
-            <div className="initext">
+            
                 <p>Carlos Oscar Charles Carlos Oscar Charles Carlos Oscar Charles Carlos Oscar Charles Carlos Oscar Charles 
                 Carlos Oscar Charles Carlos Oscar Charles Carlos Oscar Charles Carlos Oscar Charles</p>
-            </div>
+            
         </div>
     </div>
 );
